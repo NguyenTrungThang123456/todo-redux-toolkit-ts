64746c7c773293848bc7a8578dc361bae6f19f63
trungthang1610 <thangnt@reactplus.com>
2021-03-08 18:25:35 +0700

    Revert "Rename folder"
    This reverts commit ef860629db1adc2432661cc6b5b658eb7b68363d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "antd/dist/antd.css";
-import { Todos } from "./features/todo/Todo";
+import { Todos } from "./features/todos/Todo";
 import "./App.css";
 import { Layout } from "antd";
 
